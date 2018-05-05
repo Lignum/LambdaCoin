270f21495fad5d679d70e470f07f5f49adff0a92
Lignum <me@lignum.pw>
2018-05-05 19:02:24 +0200

    Oops those aren't errors

--- cli/Main.hs
+++ cli/Main.hs
@@ -11,7 +11,4 @@ main :: IO ()
 main = do
   lambdaCoinInit
   conn <- SQL.connectSqlite3 "coin.db"
-  e <- runExceptT $ initDB conn
-  case e of
-    Left e  -> putStrLn $ "Failed to initialise database. SQLite error code: " ++ show e
-    Right _ -> pure ()
+  initDB conn
--- sql/Crypto/SQL.hs
+++ sql/Crypto/SQL.hs
@@ -8,35 +8,31 @@ import Control.Monad.IO.Class
 import qualified Database.HDBC as SQL
 import qualified Database.HDBC.Sqlite3 as SQL
 
-checkError :: MonadError Integer m => Integer -> m ()
-checkError e = unless (e == toInteger SQL.sqlite_OK) $ throwError e
-
-initDB :: (MonadError Integer m, MonadIO m) => SQL.Connection -> m ()
-initDB conn = do
-  e <- liftIO $ SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `inputs` ("
-                                      , "  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
-                                      , "  `previous` BLOB NOT NULL,"
-                                      , "  `outputIndex` INTEGER NOT NULL,"
-                                      , "  `script` BLOB NOT NULL"
-                                      , ");" ]) []
-  checkError e
-  e <- liftIO $ SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `outputs` ("
-                                      , "  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
-                                      , "  `value` INTEGER NOT NULL,"
-                                      , "  `script` INTEGER NOT NULL"
-                                      , ");" ]) []
-  checkError e
-  e <- liftIO $ SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `transactionToInput` ("
-                                      , "  `transactionID` BLOB NOT NULL,"
-                                      , "  `inputID` INTEGER NOT NULL"
-                                      , ");" ]) []
-  checkError e
-  e <- liftIO $ SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `transactionToOutput` ("
-                                      , "  `transactionID` BLOB NOT NULL,"
-                                      , "  `outputID` INTEGER NOT NULL"
-                                      , ");" ]) []
-  checkError e
-  e <- liftIO $ SQL.run conn "CREATE TABLE IF NOT EXISTS `transactions` ( `id` BLOB NOT NULL UNIQUE, PRIMARY KEY(`id`) )" []
-  checkError e
-  liftIO $ SQL.commit conn
+initDB :: MonadIO m => SQL.Connection -> m ()
+initDB conn = liftIO $ do
+  SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `inputs` ("
+                        , "  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
+                        , "  `previous` BLOB NOT NULL,"
+                        , "  `outputIndex` INTEGER NOT NULL,"
+                        , "  `script` BLOB NOT NULL"
+                        , ");" ]) []
+  SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `outputs` ("
+                        , "  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
+                        , "  `value` INTEGER NOT NULL,"
+                        , "  `script` INTEGER NOT NULL"
+                        , ");" ]) []
+  SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `transactionToInput` ("
+                        , "  `id` BLOB NOT NULL,"
+                        , "  `inputID` INTEGER NOT NULL"
+                        , ");" ]) []
+  SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `transactionToOutput` ("
+                        , "  `id` BLOB NOT NULL,"
+                        , "  `outputID` INTEGER NOT NULL"
+                        , ");" ]) []
+  SQL.run conn (unlines [ "CREATE TABLE IF NOT EXISTS `transactions` ("
+                        , "  `id` BLOB NOT NULL UNIQUE,"
+                        , "  `block` INTEGER,"
+                        , "  PRIMARY KEY(`id`)"
+                        , ");" ]) []
+  SQL.commit conn
   pure ()
